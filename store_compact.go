couchbase/moss

commit f7cc198cb4a1de4d0072e8503921fd244193e251

MB-21318 - compactMaybe also counts incoming higher snapshot
Alex Gyryk showed some profiling results from his performance tests,
where the profiling was strangely showing that mossStore wasn't using
the optimized iteratorSingle implementation, even though the mossStore
was supposed to be fully compacted.  That is, a fully compacted
mossStore should have just a single segment, so the iteratorSingle
should have been utilized.

The previous commit added stats that showed there were sometimes 2
segments even after compaction.

So, this commit fixes the compactMaybe() logic to also account for the
segments that are part of the incoming higher-level Snapshot.

Change-Id: I20ffd52a508514d0dda9fe8e7adb4fd11b8a9100
Reviewed-on: http://review.couchbase.org/68771
Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
Tested-by: Steve Yen <steve.yen@gmail.com>

store_compact.go
@@ -50,15 +50,30 @@ func (s *Store) compactMaybe(higher Snapshot, persistOptions StorePersistOptions
 	defer mref.DecRef()
 
 	if compactionConcern == CompactionAllow {
+		totUpperLen := 0
 		if ss != nil && len(ss.a) >= 2 {
-			totUpperLen := 0
-			for i := 1; i < len(ss.a)-1; i++ {
+			for i := 1; i < len(ss.a); i++ {
 				totUpperLen += ss.a[i].Len()
 			}
-			pct := float64(totUpperLen) / float64(ss.a[0].Len())
-			if pct >= s.options.CompactionPercentage {
-				compactionConcern = CompactionForce
+		}
+
+		if higher != nil {
+			higherSS, ok := higher.(*segmentStack)
+			if ok {
+				higherStats := higherSS.Stats()
+				if higherStats != nil {
+					totUpperLen += int(higherStats.CurOps)
+				}
 			}
+		}
+
+		var pct float64
+		if ss != nil && len(ss.a) > 0 && ss.a[0].Len() > 0 {
+			pct = float64(totUpperLen) / float64(ss.a[0].Len())
+		}
+
+		if pct >= s.options.CompactionPercentage {
+			compactionConcern = CompactionForce
 		}
 	}
 

commit aeee0c0541a5ec0cc285e21deaa59b36bcc8f77e

MB-21477 - consider compaction only when there's >1 segment
A bug introduced in commit f7cc198cb4 around optimizing/fixing
compaction behavior turned out to overly, aggressively compact,
especially in the case when there was only a single segment.  Even
when mossStore reopened a file, for example, which was already
compacted down to a single segment, the previous code would
incorrectly decide that it was time again to compact.

Change-Id: I0beb37122fea8488b4daea6bfee763841d3faf21
Reviewed-on: http://review.couchbase.org/69218
Reviewed-by: Marty Schoch <marty.schoch@gmail.com>
Tested-by: Steve Yen <steve.yen@gmail.com>

store_compact.go
@@ -67,13 +67,15 @@ func (s *Store) compactMaybe(higher Snapshot, persistOptions StorePersistOptions
 			}
 		}
 
-		var pct float64
-		if ss != nil && len(ss.a) > 0 && ss.a[0].Len() > 0 {
-			pct = float64(totUpperLen) / float64(ss.a[0].Len())
-		}
+		if totUpperLen > 0 {
+			var pct float64
+			if ss != nil && len(ss.a) > 0 && ss.a[0].Len() > 0 {
+				pct = float64(totUpperLen) / float64(ss.a[0].Len())
+			}
 
-		if pct >= s.options.CompactionPercentage {
-			compactionConcern = CompactionForce
+			if pct >= s.options.CompactionPercentage {
+				compactionConcern = CompactionForce
+			}
 		}
 	}
 